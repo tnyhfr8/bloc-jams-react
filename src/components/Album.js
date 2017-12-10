066a1cd95f620ba19b68b45abef1870c35fa2598
tnyhfr@gmail.com <Tony@Tonys-MacBook-Air.local>
2017-12-09 22:11:08 -0800

    Checkpoint 7 Completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -10,8 +10,38 @@ class Album extends Component {
     });
 
     this.state = {
-      album: album
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
     };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
   }
 
    render() {
@@ -34,8 +64,12 @@ class Album extends Component {
              <tbody>
                  {
                    this.state.album.songs.map( (song, index) =>
-                    <tr className="song" key={index} >
-                      <td className="song-number">{ index + 1 }</td>
+                    <tr className="song" key={index} onClick={() => this.handleSongClick(song)} >
+                      <button>
+                        <span className="song-number">{index+1}</span>
+                        <span className="ion-play"></span>
+                        <span className="ion-pause"></span>
+                      </button>
                       <td className="song-title">{song.title}</td>
                       <td className="song-duration">{song.duration}</td>
                     </tr>
